91c8f1ec7b416f2c2d1687116d5687f3b7e09a25
James Irwin <tallnerd@gmail.com>
2026-02-26 15:43:44 +0100

    Make it so init is not needed for esm.

--- packages/sdk/vite.config.cjs.ts
+++ packages/sdk/vite.config.cjs.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src/core.ts'),
       name: 'PolyglotSQL',
       formats: ['cjs'],
       fileName: () => 'index.cjs',
